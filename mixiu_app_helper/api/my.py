95f92c83851906721bb734b159faa8f3d184c1ac
ckf10000 <ckf10000@sina.com>
2024-07-24 18:08:07 +0800

    first

--- mixiu_app_helper/api/my.py
+++ mixiu_app_helper/api/my.py
@@ -12,29 +12,35 @@
 from poco.proxy import UIObjectProxy
 from airtest_helper.core import DeviceApi
 from airtest_helper.platform import ANDROID_PLATFORM
+from airtest_helper.libs.extend import get_poco_factory, get_poco_child
 
 
 class MyApi(DeviceApi):
 
-    def get_my(self) -> UIObjectProxy:
+    def get_my(self, loop: int, peroid: float = 0.5, **kwargs) -> UIObjectProxy:
         d_type = ""
         name = ""
         if self.platform == ANDROID_PLATFORM:
             d_type = "android.widget.TextView"
             name = "com.mixiu.com:id/tvTabItemText4"
-        return self.get_po(d_type=d_type, name=name, text="我的")
+        options = dict(d_type=d_type, name=name, text="我的")
+        return get_poco_factory(poco=self.poco, options=options, loop=loop, peroid=peroid, **kwargs)
 
-    def touch_my(self) -> bool:
-        my_poco = self.get_my()
-        if my_poco.exists() is True:
+    def touch_my(self, loop: int, peroid: float = 0.5, **kwargs) -> bool:
+        my_poco = self.get_my(loop=loop, peroid=peroid, **kwargs)
+        if my_poco:
             my_poco.click()
             return True
         return False
 
-    def get_current_uid(self) -> str:
-        poco = self.get_po(
-            d_type="android.widget.LinearLayout", name="com.mixiu.com:id/llUidAddress"
-        ).child(name="android.widget.TextView")[1]
-        if poco.exists():
-            return poco.get_text().strip()
+    def get_current_uid(self, loop: int, peroid: float = 0.5, **kwargs) -> str:
+        options = dict(d_type="android.widget.LinearLayout", name="com.mixiu.com:id/llUidAddress")
+        parent_poco = get_poco_factory(poco=self.poco, options=options, loop=loop, peroid=peroid, **kwargs)
+        if parent_poco:
+            child_options = dict(name="android.widget.TextView")
+            child_poco = get_poco_child(
+                ui_object=parent_poco, options=child_options, child_index=1, loop=loop, peroid=peroid, **kwargs
+            )
+            if child_poco:
+                return child_poco.get_text().strip()
         return ""
