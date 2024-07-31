78c23a6627267c167a351845b94a0785e26d8358
ckf10000 <ckf10000@sina.com>
2024-07-31 11:13:51 +0800

    first

--- mixiu_app_helper/api/page/popup/__init__.py
+++ mixiu_app_helper/api/page/popup/__init__.py
@@ -5,7 +5,7 @@
 # FileName:     __init__.py
 # Description:  TODO
 # Author:       zhouhanlin
-# CreateDate:   2024/07/16
+# CreateDate:   2024/07/30
 # Copyright ©2011-2024. Hunan xxxxxxx Company limited. All rights reserved.
 # ---------------------------------------------------------------------------------------------------------
 """
