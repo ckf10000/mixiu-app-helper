6335688ef5a69589987a8616ff7ae433890b1550
ckf10000 <ckf10000@sina.com>
2024-08-03 17:27:22 +0800

    first

--- mixiu_app_helper/api/page/my/profile.py
+++ mixiu_app_helper/api/page/my/profile.py
@@ -17,7 +17,7 @@ from airtest_helper.libs.extend import get_poco_factory, get_poco_child
 
 class UiProfileApi(DeviceApi):
 
-    def get_my(self, loop: int = 1, peroid: float = 0.5, **kwargs) -> UIObjectProxy:
+    def get_my(self, loop: int = 20, peroid: float = 0.5, **kwargs) -> UIObjectProxy:
         d_type = ""
         name = ""
         if self.platform == ANDROID_PLATFORM:
@@ -26,14 +26,14 @@ class UiProfileApi(DeviceApi):
         options = dict(d_type=d_type, name=name, text="我的")
         return get_poco_factory(poco=self.poco, options=options, loop=loop, peroid=peroid, **kwargs)
 
-    def touch_my(self, loop: int = 1, peroid: float = 0.5, **kwargs) -> bool:
+    def touch_my(self, loop: int = 20, peroid: float = 0.5, **kwargs) -> bool:
         my_poco = self.get_my(loop=loop, peroid=peroid, **kwargs)
         if my_poco:
             my_poco.click()
             return True
         return False
 
-    def get_user_uid(self, loop: int = 1, peroid: float = 0.5, **kwargs) -> int or None:
+    def get_user_uid(self, loop: int = 20, peroid: float = 0.5, **kwargs) -> int or None:
         options = dict(d_type="android.widget.LinearLayout", name="com.mixiu.com:id/llUidAddress")
         parent_poco = get_poco_factory(poco=self.poco, options=options, loop=loop, peroid=peroid, **kwargs)
         if parent_poco:
@@ -46,7 +46,7 @@ class UiProfileApi(DeviceApi):
                 return int(uid_str) if isinstance(uid_str, str) and uid_str.isdigit() is True else None
         return None
 
-    def get_user_ip_location(self, loop: int = 1, peroid: float = 0.5, **kwargs) -> str or None:
+    def get_user_ip_location(self, loop: int = 20, peroid: float = 0.5, **kwargs) -> str or None:
         options = dict(d_type="android.widget.LinearLayout", name="com.mixiu.com:id/llUidAddress")
         parent_poco = get_poco_factory(poco=self.poco, options=options, loop=loop, peroid=peroid, **kwargs)
         if parent_poco:
@@ -58,7 +58,7 @@ class UiProfileApi(DeviceApi):
                 return child_poco.get_text().strip()
         return None
 
-    def get_user_diamond_balance(self, loop: int = 1, peroid: float = 0.5, **kwargs) -> int or None:
+    def get_user_diamond_balance(self, loop: int = 20, peroid: float = 0.5, **kwargs) -> int or None:
         options = dict(d_type="android.widget.TextView", name="com.mixiu.com:id/tvBlueDiamond")
         diamond_element = get_poco_factory(poco=self.poco, options=options, loop=loop, peroid=peroid, **kwargs)
         if diamond_element:
@@ -66,7 +66,7 @@ class UiProfileApi(DeviceApi):
             return int(diamond_str) if isinstance(diamond_str, str) and diamond_str.isdigit() is True else None
         return None
 
-    def get_user_mi_bean_balance(self, loop: int = 1, peroid: float = 0.5, **kwargs) -> int or None:
+    def get_user_mi_bean_balance(self, loop: int = 20, peroid: float = 0.5, **kwargs) -> int or None:
         options = dict(d_type="android.widget.TextView", name="com.mixiu.com:id/tvCatCoin")
         cat_coin_element = get_poco_factory(poco=self.poco, options=options, loop=loop, peroid=peroid, **kwargs)
         if cat_coin_element:
@@ -74,7 +74,7 @@ class UiProfileApi(DeviceApi):
             return int(cat_coin_str) if isinstance(cat_coin_str, str) and cat_coin_str.isdigit() is True else None
         return None
 
-    def get_user_fans(self, loop: int = 1, peroid: float = 0.5, **kwargs) -> int or None:
+    def get_user_fans(self, loop: int = 20, peroid: float = 0.5, **kwargs) -> int or None:
         options = dict(d_type="android.widget.TextView", name="com.mixiu.com:id/tvFansCount")
         fans_element = get_poco_factory(poco=self.poco, options=options, loop=loop, peroid=peroid, **kwargs)
         if fans_element:
@@ -82,7 +82,7 @@ class UiProfileApi(DeviceApi):
             return int(fans_str) if isinstance(fans_str, str) and fans_str.isdigit() is True else None
         return None
 
-    def get_user_follow(self, loop: int = 1, peroid: float = 0.5, **kwargs) -> int or None:
+    def get_user_follow(self, loop: int = 20, peroid: float = 0.5, **kwargs) -> int or None:
         options = dict(d_type="android.widget.TextView", name="com.mixiu.com:id/tvFollowCount")
         follow_element = get_poco_factory(poco=self.poco, options=options, loop=loop, peroid=peroid, **kwargs)
         if follow_element:
@@ -90,7 +90,7 @@ class UiProfileApi(DeviceApi):
             return int(follow_str) if isinstance(follow_str, str) and follow_str.isdigit() is True else None
         return None
 
-    def get_user_visitor(self, loop: int = 1, peroid: float = 0.5, **kwargs) -> int or None:
+    def get_user_visitor(self, loop: int = 20, peroid: float = 0.5, **kwargs) -> int or None:
         options = dict(d_type="android.widget.TextView", name="com.mixiu.com:id/tvVisitorCount")
         visitor_element = get_poco_factory(poco=self.poco, options=options, loop=loop, peroid=peroid, **kwargs)
         if visitor_element:
@@ -98,7 +98,7 @@ class UiProfileApi(DeviceApi):
             return int(visitor_str) if isinstance(visitor_str, str) and visitor_str.isdigit() is True else None
         return None
 
-    def get_user_name(self, loop: int = 1, peroid: float = 0.5, **kwargs) -> str or None:
+    def get_user_name(self, loop: int = 20, peroid: float = 0.5, **kwargs) -> str or None:
         options = dict(d_type="android.widget.TextView", name="com.mixiu.com:id/tvUserName")
         user_name_element = get_poco_factory(poco=self.poco, options=options, loop=loop, peroid=peroid, **kwargs)
         if user_name_element:
